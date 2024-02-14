2d5a82d949c78ef03a312feab3bd314f92952a57
Vũ Thanh Tùng <vuthanhtung1891995@gmail.com>
2024-02-14 10:37:49 +0000

    hi

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,10 @@ export default function Page() {
   const yesButtonSize = noCount * 20 + 16;
 
   const handleNoClick = () => {
+    if (noCount > 5) {
+      setNoCount(1);
+      return;
+    }
     setNoCount(noCount + 1);
   };
 
@@ -18,36 +22,48 @@ export default function Page() {
       "Think again!",
       "Last chance!",
       "Surely not?",
-      "You might regret this!",
-      "Give it another thought!",
-      "Are you absolutely certain?",
-      "This could be a mistake!",
-      "Have a heart!",
-      "Don't be so cold!",
-      "Change of heart?",
-      "Wouldn't you reconsider?",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
   };
+  const phrases = [
+    "https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif",
+    "https://media1.tenor.com/m/ykIUJGhcaVcAAAAC/milk-and-mocha-bear-couple.gif",
+    "https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif",
+    "https://media1.tenor.com/m/ykIUJGhcaVcAAAAC/milk-and-mocha-bear-couple.gif",
+    "https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif",
+    "https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif",
+  ];
+
+  const renderImage = () => {
+    console.log("tungvt " + noCount);
+    const imageCode = phrases[Math.min(noCount, phrases.length - 1)];
+
+    return (
+      <img
+        src={imageCode}
+        className="h-[200px]"
+        style={{ width: "400x", height: "240px" }}
+      />
+    );
+  };
 
   return (
-    <div className="centered-container">      
+    <div className="centered-container">
       <div className="valentine-container">
         {yesPressed ? (
           <>
-            <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-            <div className="text-container">Ok yay!!!</div>
+            <img src="https://media.tenor.com/qeJtDi4BOkwAAAAi/tkthao219-bubududu.gif" />
+            <div className="text-container">Nga Suxi!!!</div>
           </>
         ) : (
           <>
-            <img
+            <p>{renderImage()}</p>
+            {/* <img
               className="h-[200px]"
               style={{ width: "400x", height: "240px" }}
-              src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
-            />
+              src={{getImg()}}
+            /> */}
             <h1 className="text-container">Will you be my Valentine?</h1>
             <div>
               <button
